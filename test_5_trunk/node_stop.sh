c21262d1809fd4ada0937956aa565ea3b1804411
Andrei Savu <savu.andrei@gmail.com>
2011-02-22 11:23:56 +0200

    remove local data when killing a cluster node

--- test_5_trunk/node_stop.sh
+++ test_5_trunk/node_stop.sh
@@ -6,6 +6,7 @@ if [ -e "$PIDFILE" ]
 then
     kill -9 $(cat $PIDFILE)
     rm $PIDFILE
+    rm -f ./localhost:$1/data/version-2/*
 else
     echo "Missing $PIDFILE, not stopping respective server"
 fi
